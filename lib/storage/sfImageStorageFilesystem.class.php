1bf688cdb79dad2a26eb893355e6b76db5207f07
Kousuke Ebihara <ebihara@tejimaya.com>
2010-05-20 01:24:37 +0900

    storing and displaying images by using S3 is now supported :)

---
+++ lib/storage/sfImageStorageFilesystem.class.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file is part of the opS3ImageStoragePlugin
+ * (c) 2010 Kousuke Ebihara <ebihara@php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfImageStorageS3
+ *
+ * @package    opS3ImageStoragePlugin
+ * @subpackage storage
+ * @author     Kousuke Ebihara <ebihara@php.net>
+ */
+class sfImageStorageS3 extends sfImageStorageDefault
+{
+  const S3_SCHEME = 's3://';
+
+  static public function getUrlToImage($filename, $size, $format, $absolute = false)
+  {
+    if (self::isS3Filename($filename))
+    {
+      $rawFilename = substr($filename, strlen(self::S3_SCHEME));
+
+      $s3Filename = sfImageStorageS3::generateS3Filename($rawFilename, $format, $size);
+
+      return sfConfig::get('op_image_storage_s3_base_url').$s3Filename;
+    }
+
+    return parent::getUrlToImage($filename, $size, $format, $absolute);
+  }
+
+  public function deleteBinary()
+  {
+    $filename = $this->file->getName();
+    if (0 === strpos($filename, self::S3_SCHEME))
+    {
+      return parent::deleteBinary();
+    }
+
+    // deleting images is not supported yet
+    return null;
+  }
+
+  public static function isS3Filename($filename)
+  {
+    return (bool)(0 === strpos($filename, self::S3_SCHEME));
+  }
+
+  static public function generateS3Filename($filename, $format, $size = '')
+  {
+    $result = $filename;
+
+    if ($size)
+    {
+      $result .= '_'.$size;
+    }
+
+    return $result.'.'.$format;
+  }
+}
---
+++ lib/task/opS3ImageAddTask.class.php
@@ -0,0 +1,210 @@
+<?php
+
+/**
+ * This file is part of the opS3ImageStoragePlugin
+ * (c) 2010 Kousuke Ebihara <ebihara@php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class opS3ImageAddTask extends sfDoctrineBaseTask
+{
+  protected function configure()
+  {
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', null),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'prod'),
+
+      new sfCommandOption('limit', null, sfCommandOption::PARAMETER_REQUIRED, 'The limit', 10),
+    ));
+
+    $this->namespace        = 'opS3Image';
+    $this->name             = 'add';
+    $this->briefDescription = 'Add images to S3';
+    $this->detailedDescription = <<<EOF
+The [opS3Image:add|INFO] task adds images to S3.
+Call it with:
+
+  [./symfony opS3Image:add --limit=100|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $lockDir = sfConfig::get('sf_cache_dir').DIRECTORY_SEPARATOR.'op_s3_image_add_task.lock';
+
+    error_reporting(error_reporting() & ~(E_STRICT | E_DEPRECATED));
+
+    $databaseManager = new sfDatabaseManager($this->configuration);
+
+    $lastId = Doctrine::getTable('SnsConfig')->get('op_s3_image_add_last_id', 0);
+    $files = $this->fetchFiles($lastId, $options['limit']);
+
+    $this->lock($lockDir);
+
+    $this->logBlock('Begin uploading from id:'.$lastId, 'INFO');
+
+    foreach ($files as $file)
+    {
+      if (!$file->isImage())
+      {
+        $lastId = $file->getId();
+
+        continue;
+      }
+
+      $filename = $file->getName();
+      if (sfImageStorageS3::isS3Filename($filename))
+      {
+        $lastId = $file->getId();
+
+        continue;
+      }
+
+      $result = $this->generateImages($file);
+      if (!$result)
+      {
+        $this->logSection('NOTICE', 'Upload '.$file->getName().' is Failed.', null, 'ERROR');
+
+        break;
+      }
+
+      Doctrine::getTable('FileBin')->createQuery()
+        ->delete()
+        ->where('file_id = ?', $file->getId())
+        ->execute();
+
+      sfImageHandler::clearFileCache($filename);
+
+      $lastId = $file->getId();
+    }
+
+    Doctrine::getTable('SnsConfig')->set('op_s3_image_add_last_id', $lastId);
+    $this->logBlock('Uploadings are completed by id:'.$lastId, 'INFO');
+
+    $this->unlock($lockDir);
+  }
+
+  protected function fetchFiles($id, $limit)
+  {
+    return Doctrine::getTable('File')->createQuery()
+      ->where('id > ?', $id)
+      ->limit($limit)
+      ->execute(array(), Doctrine_Core::HYDRATE_ON_DEMAND);
+  }
+
+  protected function generateImages(File $file)
+  {
+    $formats = sfImageHandler::getAllowedFormat();
+    $sizes = array_merge(sfImageHandler::getAllowedSize(), array('raw'));
+
+    $failed = false;
+
+    foreach ($sizes as $size)
+    {
+      if ('raw' !== $size)
+      {
+        $s = explode('x', $size);
+        $width = $s[0];
+        $height = $s[1];
+      }
+      else
+      {
+        $width = $height = '';
+      }
+
+      foreach ($formats as $format)
+      {
+        $result = $this->uploadBinaryToS3($file, $format, $width, $height);
+        if ($result)
+        {
+          $this->logSection('upload+', '[id:'.$file->id.'] '.$result);
+        }
+        else
+        {
+          $message = sprintf('[id:%d] Failed to store "%s" format: %s, size: %s', $file->getId(), $file->getName(), $format, $size);
+          $this->logSection('ERROR', $message, strlen($message), 'ERROR');
+
+          $failed = true;
+
+          break;
+        }
+      }
+    }
+
+    if (!$failed)
+    {
+      $file->setName(sfImageStorageS3::S3_SCHEME.$file->getName());
+      $file->save();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  protected function uploadBinaryToS3(File $file, $format, $width, $height)
+  {
+    require_once 'Services/Amazon/S3.php';
+
+    $handler = new sfImageHandler(array(
+      'filename' => $file->getName(),
+      'format'   => $format,
+      'width'    => $width,
+      'height'   => $height,
+    ));
+
+    $account = sfConfig::get('op_image_storage_s3_account');
+    $secret = sfConfig::get('op_image_storage_s3_secret');
+    $bucket = sfConfig::get('op_image_storage_s3_bucket');
+
+    $s3 = Services_Amazon_S3::getAccount($account, $secret);
+    $bucket = $s3->getBucket($bucket);
+
+    $size = $width.'x'.$height;
+    if (!$width || !$height)
+    {
+      $size = '';
+    }
+
+    try
+    {
+      $rawBinary = $handler->getStorage()->getBinary();
+      $handler->getGenerator()->resize($rawBinary, $handler->getStorage()->getFormat());
+      $binary = $handler->getGenerator()->getBinary($handler->getStorage()->getFormat(), 100);
+    }
+    catch (Exception $e)
+    {
+      $binary = '';
+    }
+
+    if (!$binary)
+    {
+      return false;
+    }
+
+    $objectName = sfImageStorageS3::generateS3Filename($file->getName(), $format, $size);
+
+    $object = $bucket->getObject($objectName);
+    $object->contentType = $file->type;
+    $object->acl = Services_Amazon_S3_AccessControlList::ACL_PUBLIC_READ;
+    $object->data = $binary;
+    $object->save();
+
+    return $objectName;
+  }
+
+  protected function lock($dir)
+  {
+    if (!@mkdir($dir))
+    {
+      throw new RuntimeException('Unable to lock');
+    }
+  }
+
+  protected function unlock($dir)
+  {
+    @rmdir($dir);
+  }
+}
